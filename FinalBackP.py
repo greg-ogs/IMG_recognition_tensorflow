d8c4791cb3b0bf2a864f69f4cb6b6294adb784f9
alex <alexorogom@gmail.com>
2023-02-08 21:36:36 -0600

    Orden

--- FinalBackP.py
+++ FinalBackP.py
@@ -11,7 +11,6 @@ import numpy as np
 import os
 import PIL
 import tensorflow as tf
-
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
